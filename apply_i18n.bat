347e06dab6f9441f9e0f8438d2623904556bf5ee
Tarvitz <tarvitz@blacklibrary.ru>
2016-01-06 04:04:46 +0300

    [data] add scripts for data unpack and pack translated

---
+++ apply_i18n.bat
@@ -0,0 +1,2 @@
+@echo on
+python tools/apply_i18n.py -d D:/Steam/steamapps/common/Underrail/Data/_Dialogs -T data/i18n -o data/dialogs/ -S
---
+++ unpack_i18n.bat
@@ -0,0 +1,2 @@
+@echo on
+python tools/dump_i18n.py -d D:/Steam/steamapps/common/Underrail/Data/_Dialogs -o data/i18n -S
